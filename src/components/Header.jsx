78dd80dcb6eb6f24dfef2ea673e7c51ab86b97aa
Razibul Islam <dev.ronib@gmail.com>
2024-05-14 00:12:36 +0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
 import {Button} from "@/components/ui/button.jsx";
-import {Link, NavLink} from "react-router-dom";
+import {Link, NavLink, useNavigate} from "react-router-dom";
 import {RiMenuFill} from "react-icons/ri";
 import {useState} from "react";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import useAuth from "@/hooks/useAuth.jsx";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 const Header = () => {
+    const {user, logOut} = useAuth();
+    const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => setIsOpen(!isOpen);
 
+    const handleLogout = async () => {
+        await logOut()
+            .then(() => {
+                navigate('/login');
+                toast.error("Logged out successfully.")
+            })
+            .catch((error) => toast.error(error.message))
+    }
+
     return (
-        <header className='bg-base-100 shadow'>
-            <div className='w-[98%] lg:w-10/12 max-w-[1275px] mx-auto sticky top-0 flex justify-between items-center p-2 lg:p-3'>
+        <header className='bg-slate-50 sticky top-0 z-10 shadow'>
+            <div className='w-[98%] lg:w-10/12 max-w-[1275px] mx-auto flex justify-between items-center p-2 lg:p-3'>
                 <div className='flex items-center gap-2'>
                     <div className='lg:hidden relative'>
                         <Button onClick={toggleMenu}><RiMenuFill /></Button>
@@ -21,7 +37,7 @@ const Header = () => {
                             <NavLink to='/request'>My Request</NavLink>
                         </div>
                     </div>
-                    <Link to='/' className='text-xl lg:text-3xl font-lexend'>
+                    <Link to='/' className='text-xl lg:text-3xl font-black font-lexend'>
                         Food Hub
                     </Link>
                 </div>
@@ -32,15 +48,33 @@ const Header = () => {
                     <NavLink to='/manage'>Manage Foods</NavLink>
                     <NavLink to='/request'>My Request</NavLink>
                 </div>
-                <div className='flex gap-2'>
-                    <Button asChild>
-                        <Link to='/login'>Login</Link>
-                    </Button>
-                    <Button asChild>
-                        <Link to='/register'>Sign up</Link>
-                    </Button>
+                <div className='flex gap-4'>
+                    {
+                        user ? (
+                            <>
+                                <Avatar>
+                                    <AvatarImage src={user.photoURL} />
+                                    <AvatarFallback>{user.displayName}</AvatarFallback>
+                                </Avatar>
+                                <Button onClick={handleLogout}>Log out</Button>
+                            </>
+                        ) : (
+                            <>
+                                <Button asChild>
+                                    <Link to='/login'>Login</Link>
+                                </Button>
+                                <Button asChild>
+                                    <Link to='/register'>Sign up</Link>
+                                </Button>
+                            </>
+                        )
+                    }
                 </div>
             </div>
+            <ToastContainer
+                autoClose={2000}
+                theme="colored"
+            />
         </header>
     );
 };
