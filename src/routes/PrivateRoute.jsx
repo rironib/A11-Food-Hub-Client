6d3e34a82a527a67f564cbec1a054d6526016629
Razibul Islam <dev.ronib@gmail.com>
2024-05-14 00:14:00 +0600

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,28 @@
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import {Navigate, useLocation} from "react-router-dom";
+import PropTypes from "prop-types";
+import useAuth from "../hooks/useAuth.jsx";
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useAuth();
+    const location = useLocation();
+
+    if (loading) {
+        return (
+            <div className='flex justify-center items-center'>
+                <span className="loading loading-ring loading-lg"></span>
+            </div>
+        );
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate state={location.pathname} to='/login'/>;
 };
+
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+}
 
 export default PrivateRoute;
