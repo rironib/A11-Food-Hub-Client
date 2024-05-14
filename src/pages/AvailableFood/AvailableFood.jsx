5088500cc417749eab21993c3a686705ef7528af
Razibul Islam <dev.ronib@gmail.com>
2024-05-14 11:29:43 +0600

    Update AvailableFood.jsx

--- src/pages/AvailableFood/AvailableFood.jsx
+++ src/pages/AvailableFood/AvailableFood.jsx
@@ -2,7 +2,7 @@ import {Helmet} from "react-helmet-async";
 import {useLoaderData} from "react-router-dom";
 import Card from "@/components/Card.jsx";
 import {Button} from "@/components/ui/button.jsx";
-import {RiSortAsc, RiSortDesc} from "react-icons/ri";
+import {RiLayoutGrid2Fill, RiLayoutGridFill, RiSortAsc, RiSortDesc} from "react-icons/ri";
 import {useState} from "react";
 
 const AvailableFood = () => {
@@ -10,6 +10,9 @@ const AvailableFood = () => {
     const filteredFoods = foods.filter(food => food.status === 'Available');
     const [items, setItems] = useState(filteredFoods);
     const [searchTerm, setSearchTerm] = useState("");
+    const [grid, setGrid] = useState(false);
+
+    const handleGrid = () => setGrid(!grid);
 
     const handleSearch = e => {
         setSearchTerm(e.target.value);
@@ -45,20 +48,36 @@ const AvailableFood = () => {
                     <p className='text-slate-700 mb-8'>Explore mouthwatering Available Foods - a curated selection of
                         culinary delights awaiting your discovery and enjoyment.</p>
                 </div>
-                <div className='flex justify-between my-4'>
+                <div className='sm:hidden'>
                     <input
                         type="text"
                         placeholder="Search by name"
                         value={searchTerm}
                         onChange={handleSearch}
-                        className='p-3 border rounded-lg outline-none'
+                        className='w-full p-3 border rounded-lg outline-none'
                     />
+                </div>
+                <div className='flex justify-between items-center gap-4 mt-6 mb-8'>
+                    <div className='flex justify-end gap-4 text-2xl'>
+                        <Button onClick={handleGrid} disabled={`${grid ? 'disabled' : ''}`}><RiLayoutGridFill/></Button>
+                        <Button onClick={handleGrid}
+                                disabled={`${!grid ? 'disabled' : ''}`}><RiLayoutGrid2Fill/></Button>
+                    </div>
+                    <div className='hidden sm:block'>
+                        <input
+                            type="text"
+                            placeholder="Search by name"
+                            value={searchTerm}
+                            onChange={handleSearch}
+                            className='md:w-80 xl:w-96 p-3 border rounded-lg outline-none'
+                        />
+                    </div>
                     <div className='flex justify-end gap-4 text-2xl'>
                         <Button onClick={() => handleSorting('asc')} disabled={asc}><RiSortAsc/></Button>
                         <Button onClick={() => handleSorting('dsc')} disabled={dsc}><RiSortDesc/></Button>
                     </div>
                 </div>
-                <div className={`grid grid-cols-3 gap-5`}>
+                <div className={`grid ${grid ? 'md:grid-cols-2 md:gap-12' : 'grid-cols-2 md:grid-cols-3'} gap-5`}>
                     {
                         items.map(food => <Card key={food._id} food={food}/>)
                     }
