f155690d9cd5dbf36ad9b4a16c4159399217a593
Razibul Islam <dev.ronib@gmail.com>
2024-05-30 12:45:52 +0600

    UPDATED: Print data of manage food

--- src/pages/ManageFood/ManageFood.jsx
+++ src/pages/ManageFood/ManageFood.jsx
@@ -2,37 +2,45 @@ import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/c
 import {Link} from "react-router-dom";
 import {RiDeleteBin7Line, RiPencilFill} from "react-icons/ri";
 import useAuth from "@/hooks/useAuth.jsx";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
 import {Helmet} from "react-helmet-async";
 import useAxiosSecure from "@/hooks/useAxiosSecure.jsx";
 import ReactPaginate from 'react-paginate';
+import { PDFExport } from '@progress/kendo-react-pdf';
 
 const ManageFood = () => {
+    // Fetching user info for authentication
     const {user} = useAuth();
+
+    // State management
     const [items, setItems] = useState([]);
     const [totalItems, setTotalItems] = useState(0);
     const [currentPage, setCurrentPage] = useState(0);
-    const [itemsPerPage] = useState(10);
-    const pageCount = Math.ceil(totalItems / itemsPerPage);
+    const [itemsPerPage] = useState(10); // Number of items per page
+    const pageCount = Math.ceil(totalItems / itemsPerPage); // Total number of pages
     const axiosSecure = useAxiosSecure();
+    const componentRef = useRef(null);
 
+    // URL for fetching food items based on user and pagination
     const url = `/manage?email=${user?.email}&page=${currentPage}&limit=${itemsPerPage}`;
 
+    // Fetching food items when component mounts or when URL or axiosSecure changes
     useEffect(() => {
         axiosSecure.get(url)
             .then(res => {
-                console.log(res.data)
                 setItems(res.data?.items);
                 setTotalItems(res.data?.totalItems);
             })
     }, [url, axiosSecure]);
 
+    // Function to handle page change in pagination
     const handlePageClick = (data) => {
         setCurrentPage(data.selected);
     }
 
+    // Function to handle deletion of a food item
     const handleDelete = (id) => {
         Swal.fire({
             title: "Are you sure?",
@@ -45,7 +53,7 @@ const ManageFood = () => {
         }).then((result) => {
             if (result.isConfirmed) {
 
-                // Using axios
+                // Using axios to send DELETE request to delete the food item
                 axios.delete(`https://food-hub-api-orpin.vercel.app/foods/${id}`)
                     .then(data => {
                         const result = data.data;
@@ -55,6 +63,7 @@ const ManageFood = () => {
                                 text: "Food deleted successfully.",
                                 icon: "success"
                             });
+                            // Updating state to reflect deletion
                             const remaining = items.filter(food => food._id !== id);
                             setItems(remaining);
                         }
@@ -70,64 +79,78 @@ const ManageFood = () => {
         });
     }
 
-    console.log(items)
-
     return (
         <>
             <Helmet>
                 <title>Food Hub | Manage Foods</title>
             </Helmet>
-            <div className="w-full mt-12 mb-20">
-                <h2 className='font-bold text-3xl text-center mb-6'>Manage Foods</h2>
-                <div className="mb-8 rounded-md border">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>Name</TableHead>
-                                <TableHead>Quantity</TableHead>
-                                <TableHead>Location</TableHead>
-                                <TableHead>Expiration</TableHead>
-                                <TableHead>Status</TableHead>
-                                <TableHead>Donor</TableHead>
-                                <TableHead>Actions</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {
-                                items.map((food) => (
-                                    <TableRow key={food._id}>
-                                        <TableCell>{food.name}</TableCell>
-                                        <TableCell>{food.quantity}</TableCell>
-                                        <TableCell>{food.location}</TableCell>
-                                        <TableCell>{food.expire}</TableCell>
-                                        <TableCell>{food.status}</TableCell>
-                                        <TableCell>{food.donorName}</TableCell>
-                                        <TableCell className='flex gap-2 flex-wrap'>
-                                            <Link to={`/update/${food._id}`}
-                                                  className='bg-slate-900 p-2 px-4 text-white text-base rounded'><RiPencilFill/></Link>
-                                            <button onClick={() => handleDelete(food._id)}
-                                                    className='bg-red-600 p-2 px-4 text-white text-base rounded'>
-                                                <RiDeleteBin7Line/></button>
-                                        </TableCell>
-                                    </TableRow>
-                                ))
-                            }
-                        </TableBody>
-                    </Table>
+            <div className='mt-12 mb-20'>
+                <div className='text-right mb-2'>
+                    <button onClick={() => componentRef.current.save()}
+                            className='px-6 py-2 bg-black text-white font-medium rounded'>
+                        Export to PDF
+                    </button>
                 </div>
-                <ReactPaginate
-                    previousLabel={"Previous"}
-                    nextLabel={"Next"}
-                    breakLabel={"..."}
-                    breakClassName={"break-me"}
-                    pageCount={pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={handlePageClick}
-                    containerClassName={"pagination"}
-                    subContainerClassName={"border"}
-                    activeClassName={"active"}
-                />
+                <PDFExport ref={componentRef} author={user.displayName} fileName={'manage-foods'} title={'Manage Foods'} margin="2cm">
+                    <div className="w-full">
+                        <h2 className='font-bold text-3xl text-center mb-6'>Manage Foods</h2>
+                        <div className="mb-8 rounded-md border">
+                            {/* Table for displaying food items */}
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        {/* Table headers */}
+                                        <TableHead>Name</TableHead>
+                                        <TableHead>Quantity</TableHead>
+                                        <TableHead>Location</TableHead>
+                                        <TableHead>Expiration</TableHead>
+                                        <TableHead>Status</TableHead>
+                                        <TableHead>Donor</TableHead>
+                                        <TableHead>Actions</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {/* Rendering each food item */}
+                                    {
+                                        items.map((food) => (
+                                            <TableRow key={food._id}>
+                                                <TableCell>{food.name}</TableCell>
+                                                <TableCell>{food.quantity}</TableCell>
+                                                <TableCell>{food.location}</TableCell>
+                                                <TableCell>{food.expire}</TableCell>
+                                                <TableCell>{food.status}</TableCell>
+                                                <TableCell>{food.donorName}</TableCell>
+                                                <TableCell className='flex gap-2 flex-wrap'>
+                                                    {/* Edit button */}
+                                                    <Link to={`/update/${food._id}`}
+                                                          className='bg-slate-900 p-2 px-4 text-white text-base rounded'><RiPencilFill/></Link>
+                                                    {/* Delete button */}
+                                                    <button onClick={() => handleDelete(food._id)}
+                                                            className='bg-red-600 p-2 px-4 text-white text-base rounded'>
+                                                        <RiDeleteBin7Line/></button>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))
+                                    }
+                                </TableBody>
+                            </Table>
+                        </div>
+                        {/* Pagination component */}
+                        <ReactPaginate
+                            previousLabel={"Previous"}
+                            nextLabel={"Next"}
+                            breakLabel={"..."}
+                            breakClassName={"break-me"}
+                            pageCount={pageCount}
+                            marginPagesDisplayed={2}
+                            pageRangeDisplayed={5}
+                            onPageChange={handlePageClick}
+                            containerClassName={"pagination"}
+                            subContainerClassName={"border"}
+                            activeClassName={"active"}
+                        />
+                    </div>
+                </PDFExport>
             </div>
         </>
     )
